05bbfdb921f12246a629cb6a978ea1dc3e1737b5
JonnathanLenovo <andres.652@hotmail.com>
2016-08-25 07:16:08 -0500

    Cambio de algunos iconos

--- app/src/main/java/com/jari/example/navigationdrawer/Fragments/FragmentEmpleoPorConvocatoria.java
+++ app/src/main/java/com/jari/example/navigationdrawer/Fragments/FragmentEmpleoPorConvocatoria.java
@@ -28,7 +28,6 @@ import retrofit2.Retrofit;
  * Created by Jonnathan on 15/08/2016.
  */
 public class FragmentEmpleoPorConvocatoria extends Fragment {
-    public static final String TAG = "SIMO";
     private static Retrofit retrofit = null;
     RecyclerView _recyclerView;
     List<EmpleoPorConvocatoria> _listaEmpleoPorConvocatoria;
@@ -41,7 +40,7 @@ public class FragmentEmpleoPorConvocatoria extends Fragment {
         View view = inflater.inflate(R.layout.fragment_empleos_x_convocatoria, container, false);
         _recyclerView = (RecyclerView) view.findViewById(R.id.empleosRecyclerView);
         _recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        cargarEmpleos("datos", MainActivity.NUMERO_REGISTROS);
+        cargarEmpleos();
         return view;
     }
 
@@ -50,18 +49,18 @@ public class FragmentEmpleoPorConvocatoria extends Fragment {
         super.onStart();
     }
 
-    private void cargarEmpleos(String palabraClave, int numeroRegistros) {
+    private void cargarEmpleos() {
         ApiInterfaceEmpleos apiService = ApiClientEmpleos.getClient().create(ApiInterfaceEmpleos.class);
         Call<List<EmpleoPorConvocatoria>> call = apiService.getEmpleosPorConvocatorias();
         call.enqueue(new Callback<List<EmpleoPorConvocatoria>>() {
             @Override
             public void onResponse(Call<List<EmpleoPorConvocatoria>> call, Response<List<EmpleoPorConvocatoria>> response) {
 
-                List<EmpleoPorConvocatoria> listaObjetos =  response.body(); // response.body().getResults();
+                List<EmpleoPorConvocatoria> listaObjetos =  response.body();
                 _listaEmpleoPorConvocatoria = listaObjetos;
 
-                _recyclerView.setAdapter(new EmpleoPorConvocatoriaAdapter(_listaEmpleoPorConvocatoria, R.layout.list_item_empleo, getActivity().getApplicationContext()));
-                Log.d(TAG, "ResultadoOfertaPublica: " + listaObjetos.size());
+                _recyclerView.setAdapter(new EmpleoPorConvocatoriaAdapter(_listaEmpleoPorConvocatoria, getActivity().getApplicationContext()));
+                Log.d(getContext().getString(R.string.tag), "EmpleoPorConvocatoria: " + listaObjetos.size());
             }
 
             @Override
@@ -69,31 +68,6 @@ public class FragmentEmpleoPorConvocatoria extends Fragment {
 
             }
         });
-
-
-//        ApiInterface apiService = ApiClient.getClientOfertaPublica().create(ApiInterface.class);
-//
-//        Call<List<ResultadoOfertaPublica>> call = apiService.getOfertaPublica(palabraClave, numeroRegistros);
-//        call.enqueue(new Callback<List<ResultadoOfertaPublica>>() {
-//            @Override
-//            public void onResponse(Call<List<ResultadoOfertaPublica>>call, Response<List<ResultadoOfertaPublica>> response) {
-//                int n = 8;
-//
-//
-//                List<ResultadoOfertaPublica> listaObjetos =  response.body(); // response.body().getResults();
-//                _listaEmpleoPorConvocatoria = listaObjetos;
-//
-//                _recyclerView.setAdapter(new EmpleoAdapter(_listaEmpleoPorConvocatoria, R.layout.list_item_empleo, getActivity().getApplicationContext()));
-//                Log.d(TAG, "ResultadoOfertaPublica: " + listaObjetos.size());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<ResultadoOfertaPublica>>call, Throwable t) {
-//                // Log error here since request failed
-//                Log.e(TAG, t.toString());
-//            }
-//        });
-
 
     }
 
